609bcf812679981ea57764e67964ec470c7ef1eb
Insignate <46281639+Insignate@users.noreply.github.com>
2020-07-29 22:43:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-
+var ashScreamAudios = '';
+var randomNumber = 0;
 function addValue(){
+  
+  ashScreamAudios = document.getElementsByClassName("ashAudios");
+  ashScreamAudios[getRandomInt(ashScreamAudios.length)].play();
+  
   var element = document.getElementById('counter');
   element.innerText = parseInt(element.innerText) + 1;
+  
 }
 
 function addDanyValue(){
   var element = document.getElementById('counterDany');
   element.innerText = parseInt(element.innerText) + 1;
+}
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
