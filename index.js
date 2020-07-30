b873d9b14755ff2d425e62496ca9f355c086b2ea
Insignate <46281639+Insignate@users.noreply.github.com>
2020-07-29 21:04:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
 function addValue(){
-  
   var element = document.getElementById('counter');
   element.innerText = parseInt(element.innerText) + 1;
+}
 
+function addDanyValue(){
+  var element = document.getElementById('counterDany');
+  element.innerText = parseInt(element.innerText) + 1;
 }
