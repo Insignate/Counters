38c36f1a19a1dc6f289f86d0e9567d25c23ce355
Insignate <46281639+Insignate@users.noreply.github.com>
2020-07-29 23:15:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var ashScreamAudios = '';
-var randomNumber = 0;
+
 function addValue(){
   
-  ashScreamAudios = document.getElementsByClassName("ashAudios");
+  var ashScreamAudios = document.getElementsByClassName("ashAudios");
   ashScreamAudios[getRandomInt(ashScreamAudios.length)].play();
   
   var element = document.getElementById('counter');
@@ -11,6 +10,10 @@ function addValue(){
 }
 
 function addDanyValue(){
+  
+  danyFucksAudios = document.getElementsByClassName("danyFucks");
+  danyFucksAudios[getRandomInt(danyFucksAudios.length)].play();
+  
   var element = document.getElementById('counterDany');
   element.innerText = parseInt(element.innerText) + 1;
 }
